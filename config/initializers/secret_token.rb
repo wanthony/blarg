29d3f984274879de575f599315e423af4bc9be86
Anthony White <anthony.white.j@gmail.com>
2011-09-28 20:26:15 -0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blarg::Application.config.secret_token = '4cbfe9323fa2ad1e70d13d0f6c9a8a1c93a1ea02ee5305041bf60980df30bc1ab70af7dd0c506dafe3a28e067c56284e621f87be28b3a7bb73accf69520e8b0d'
